d3022d715761d9f7602d3c3099c234080bc174a1
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-11-16 00:17:35 -0500

    add serverless functions

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -12,7 +12,7 @@ const DashBoard = () => {
   const [blogPost, setBlogPost] = useState({});
 
   useEffect(() => {
-    fetch("https://dev.to/api/articles?username=din0saur5")
+    fetch("/.netlify/functions/api")
       .then((response) => response.json())
       .then((data) => {
         const {canonical_url, title, description, tag_list, reading_time_minutes, readable_publish_date} = data[0]
